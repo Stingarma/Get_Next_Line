ab9c35409eb448068ba7fdc1abe3112db426edcc
Stingarma <144959747+Stingarma@users.noreply.github.com>
2023-10-28 16:36:44 +0200

    MAJ 28.10.2023 16h30

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: lsaumon <lsaumon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 10:53:17 by lsaumon           #+#    #+#             */
-/*   Updated: 2023/10/28 15:58:47 by lsaumon          ###   ########.fr       */
+/*   Updated: 2023/10/28 16:35:39 by lsaumon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ size_t	ft_strlen(const char *s)
 		i++;
 	return (i);
 }
+
 void	ft_bzero(void *s, size_t n)
 {
 	size_t	i;
@@ -33,6 +34,7 @@ void	ft_bzero(void *s, size_t n)
 	}
 	return (s);
 }
+
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
@@ -40,16 +42,17 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i] != (unsigned char)c)
 	{
-		if (s[i] == '\0');
+		if (s[i] == '\0')
 			return ((char *)0);
 		i++;
 	}
 	return ((char *) &s[i]);
 }
+
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*tab;
-	
+
 	if (!nmemb || !size)
 		return (malloc(0));
 	if ((nmemb * size) / size != nmemb)
@@ -60,7 +63,31 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	ft_memset(tab, 0, nmemb * size);
 	return (tab);
 }
+
 char	*ft_strjoin(const char *s1, const char *s2)
 {
-	
+	size_t	len_s1;
+	size_t	len_s2;
+	size_t	len_result;
+	char	*result;
+
+	len_s1 = 0;
+	len_result = ft_strlen(s1) + ft_strlen(s2);
+	result = ft_calloc(len_result + 1, sizeof(char));
+	if (!result || !s1 || !s2)
+		return (NULL);
+	while (s1[len_s1])
+	{
+		result = s1[len_s1];
+		len_s1++;
+	}
+	len_s2 = 0;
+	while (s2[len_s2])
+	{
+		result[len_s1] = s2[len_s2];
+		len_s1++;
+		len_s2++;
+	}
+	result[len_result] = 0;
+	return (result);
 }
